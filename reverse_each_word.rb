c918edeabeaff067f56091c97447a3cb2f6a0b85
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-12 03:12:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,7 +6,7 @@
 
 
 def reverse_each_word(sentence)
-  reversed_sentence = sentence.split.collect do [word]
+  reversed_sentence = sentence.split.collect do |word|
   word.reverse
 end
 reversed.join(" ")
